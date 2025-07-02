780746a85f649db6f845fdbb3e34f55c78b8531e
alvcris <cristiano.alves.ca@gmail.com>
2025-07-02 14:13:41 -0300

    Enforcing code style by adding maven-checkstyle-plugin

--- payroll-api/src/main/java/com/example/payroll/adapters/outbound/persistence/contract/mapper/ContractMapper.java
+++ payroll-api/src/main/java/com/example/payroll/adapters/outbound/persistence/contract/mapper/ContractMapper.java
@@ -4,11 +4,13 @@ import com.example.payroll.adapters.outbound.persistence.contract.entity.Contrac
 import com.example.payroll.config.MapperConfiguration;
 import com.example.payroll.domain.contract.model.Contract;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(config = MapperConfiguration.class)
 public abstract class ContractMapper {
 
     public abstract Contract fromEntity(ContractEntity entity);
 
+    @Mapping(target = "employeeId", ignore = true)
     public abstract ContractEntity fromContract(Contract contract);
 }
--- payroll-api/src/main/java/com/example/payroll/adapters/outbound/persistence/payroll/mapper/PayrollMapper.java
+++ payroll-api/src/main/java/com/example/payroll/adapters/outbound/persistence/payroll/mapper/PayrollMapper.java
@@ -4,10 +4,13 @@ import com.example.payroll.adapters.outbound.persistence.payroll.entity.PayrollE
 import com.example.payroll.config.MapperConfiguration;
 import com.example.payroll.domain.payroll.model.Payroll;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(config = MapperConfiguration.class, uses = DeductionsMapper.class)
 public abstract class PayrollMapper {
 
+    @Mapping(target = "employee.assignBankAccount", ignore = true)
+    @Mapping(target = "employee.assignContracts", ignore = true)
     public abstract Payroll fromEntity(PayrollEntity payrollEntity);
 
 }
